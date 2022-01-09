a5c3b6942563b2f686c1d54a2f86e33797e2f2c0
Peanut Arbuckle <theraddad1629@gmail.com>
2022-01-09 02:27:10 -0800

    updated for full ERC20 compliance

--- erc20.sol
+++ erc20.sol
@@ -7,12 +7,12 @@ interface ERC20Interface{
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function transfer(address to, uint tokens) external returns (bool success);
 
-    // function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    // function approve(address spender, uint tokens) external returns (bool success);
-    // function transferFrom(address from, address to, uint tokens) external returns (bool success);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
 
     event Transfer(address indexed from, address indexed to, uint tokens);
-    // event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
 contract NutBux is ERC20Interface{
@@ -24,6 +24,11 @@ contract NutBux is ERC20Interface{
     address public founder;
     mapping(address => uint) public balances;
     // balances[0x111...] = 100;
+
+    mapping(address => mapping(address => uint)) allowed;
+
+    //0x111... (owner) allows 0x2222....(the spender) ---- 100 tokens
+    //allowed[[0x111][0x222] = 100;]
 
     constructor(){
         totalSupply = 1000000;
@@ -41,6 +46,31 @@ contract NutBux is ERC20Interface{
         balances[to] += tokens;
         balances[msg.sender] -= tokens;
         emit Transfer(msg.sender, to, tokens);
+
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) view public override returns(uint){
+        return allowed[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint tokens) public override returns (bool success){
+        require(balances[msg.sender] >= tokens);
+        require(tokens > 0);
+
+        allowed[msg.sender][spender] = tokens;
+        
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public override returns (bool success){
+        require(allowed[from][to] >= tokens);
+        require(balances[from] >= tokens);
+
+        balances[from] -= tokens;
+        balances[to] += tokens;
+        allowed[from][to] -= tokens;
 
         return true;
     }
