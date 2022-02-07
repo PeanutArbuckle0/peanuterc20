e8b92b40f2d3caaac2d77788d61fe0e32c3a474e
Peanut Arbuckle <theraddad1629@gmail.com>
2022-02-07 17:36:15 -0600

    added tests for transfer functions

--- test/erc20.js
+++ test/erc20.js
@@ -36,7 +36,17 @@ describe("Token contract", function () {
         it("Should revert if allowance is lower than attempted spend", async function() {
             await hhNutBux.approve(addr1.address, 4999);
             await expect(hhNutBux.transferFrom(founder.address, addr1.address, 5000)).to.be.reverted;
-            console.log()
+        });
+    });
+    describe("Transfer functions", function () {
+        it("Should be able to send tokens to another wallet", async function () {
+            await hhNutBux.transfer(addr1.address, 999);
+            expect(await hhNutBux.balanceOf(addr1.address)).to.equal(999);
+        });
+        it("Should be able to transfer approved spend", async function () {
+            await hhNutBux.approve(addr1.address, 5000)
+            await hhNutBux.transferFrom(founder.address, addr1.address, 5000);
+            expect(await hhNutBux.balanceOf(addr1.address)).to.equal(5000);
         });
     });
 });
